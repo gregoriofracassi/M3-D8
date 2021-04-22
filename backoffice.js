9ad93babc94be3c7ce7ed6de608d4b92376d03db
gregoriofracassi <gregorio.fracassi@gmail.com>
2021-04-22 16:50:09 +0200

    bettered backoffice

--- backoffice.js
+++ backoffice.js
@@ -1,4 +1,23 @@
 const submit = document.querySelector('.submit')
+const urlParams = new URLSearchParams(window.location.search);
+const product_id = urlParams.get('id')
+const endpoint = product_id ? `https://striveschool-api.herokuapp.com/api/product/${product_id}` : "https://striveschool-api.herokuapp.com/api/product/"
+const method = product_id ? "PUT" : "POST"
+
+console.log(endpoint, method)
+
+window.onload = async () => {
+    const response = await fetch(endpoint, {
+        headers: { "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMTQ4Y2IxZjBmYjAwMTVkOTE3NWYiLCJpYXQiOjE2MTkwMDc1MjQsImV4cCI6MTYyMDIxNzEyNH0.81RRvTd2IqaPZsKsv6VfBMJBnL17zGR0wW-6hEZVefU"}
+    })
+    const respBody = await response.json()
+    
+    document.querySelector('#name').value = respBody.name
+    document.querySelector('#description').value = respBody.description
+    document.querySelector('#brand').value = respBody.brand
+    document.querySelector('#imageUrl').value = respBody.imageUrl
+    document.querySelector('#price').value = respBody.price
+}
 
 const postData = async (e) => {
     e.preventDefault()
@@ -11,16 +30,25 @@ const postData = async (e) => {
         price: document.querySelector('#price').value
     }
 
-    const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", { 
-        method: 'POST',
-        headers: { "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMTQ4Y2IxZjBmYjAwMTVkOTE3NWYiLCJpYXQiOjE2MTkwMDc1MjQsImV4cCI6MTYyMDIxNzEyNH0.81RRvTd2IqaPZsKsv6VfBMJBnL17zGR0wW-6hEZVefU",
-                    "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newProduct)
-    })
+    try {
+        const response = await fetch(endpoint, { 
+            method: method,
+            headers: { "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMTQ4Y2IxZjBmYjAwMTVkOTE3NWYiLCJpYXQiOjE2MTkwMDc1MjQsImV4cCI6MTYyMDIxNzEyNH0.81RRvTd2IqaPZsKsv6VfBMJBnL17zGR0wW-6hEZVefU",
+                        "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newProduct)
+        })  
+        if(!response.ok) {
+            throw new Error('something went wrong')
+        }
+        const postResponse = await response.json()
+        console.log(postResponse)
+        alert(`successfully added product ${postResponse._id}`)
+    } catch (error) {
+        console.log(error)
+        alert(error.message)
+    }
 
-    const postResponse = await response.json()
-    console.log(postResponse)
 }
 
 submit.onsubmit = postData
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const row = document.querySelector('.row')
+const deleters = []
 
 const getApiBody = async () => {
     const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", { headers: { "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMTQ4Y2IxZjBmYjAwMTVkOTE3NWYiLCJpYXQiOjE2MTkwMDc1MjQsImV4cCI6MTYyMDIxNzEyNH0.81RRvTd2IqaPZsKsv6VfBMJBnL17zGR0wW-6hEZVefU"}})
@@ -12,19 +13,56 @@ const displayProducts = async () => {
     row.innerHTML = products.map(
         pr => {
             return         `
-            <a href="product-page.html?id=${pr._id}"
+            <a href="product-page.html?id=${pr._id}">
             <div class="col-3">
             <div class="card p-2">
                 <img class="img-fluid card-img-top" src="${pr.imageUrl}" alt="Card image cap">
                 <div class="card-body">
                     <h5>${pr.brand}</h5>
                     <p class="card-text">${pr.name}</p>
+                    <a href="backoffice.html?id=${pr._id}">Edit</a> | <p class="delete d-inline pointer" onclick="deleteProduct('${pr._id}')">Delete</p>
                 </div>
             </div>
             </div>
+            </a>
             `
         }
     ).join('')
+   
 }
+
+
+const deleteProduct = async (id) => {
+    console.log(id);
+
+    try {
+        const response = await fetch (`https://striveschool-api.herokuapp.com/api/product/${id}`, {
+            method: "DELETE",
+            headers: { "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDgwMTQ4Y2IxZjBmYjAwMTVkOTE3NWYiLCJpYXQiOjE2MTkwMDc1MjQsImV4cCI6MTYyMDIxNzEyNH0.81RRvTd2IqaPZsKsv6VfBMJBnL17zGR0wW-6hEZVefU",
+                            "Content-Type": "application/json"
+                }
+        })
+        if (!response.ok) {
+            throw new Error('endpoint isnt working properly')
+        }
+        const responseBody = response.json()
+        alert(`succesfully deleted product ${id}`)
+    } catch (error) {
+        console.log(error)
+        alert(error.message)
+    }
+    location.assign('index.html')
+}
+/*
+const assignDelete = () => {
+    let deleters = document.querySelectorAll('.delete')
+    deleters.forEach( del => {
+        del.onclick = () => deleteProduct(del.dataset.id)
+    })
+    console.log(deleters);
+}
+
+*/
+
 
 window.onload = displayProducts
